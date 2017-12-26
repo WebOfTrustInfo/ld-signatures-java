1f8d060bb660af87605a6a2555d2b33afe9fe92a
Markus Sabadello <markus@danubetech.com>
2017-12-26 15:03:07 +0400

    bump jsonld-java 0.11.1

--- src/main/java/info/weboftrust/ldsignatures/util/CanonicalizationUtil.java
+++ src/main/java/info/weboftrust/ldsignatures/util/CanonicalizationUtil.java
@@ -1,5 +1,6 @@
 package info.weboftrust.ldsignatures.util;
 
+import com.github.jsonldjava.core.JsonLdConsts;
 import com.github.jsonldjava.core.JsonLdError;
 import com.github.jsonldjava.core.JsonLdOptions;
 import com.github.jsonldjava.core.JsonLdProcessor;
@@ -15,7 +16,7 @@ public class CanonicalizationUtil {
 		// canonicalize the JSON-LD
 
 		JsonLdOptions options = new JsonLdOptions();
-		options.format = "application/nquads";
+		options.format = JsonLdConsts.APPLICATION_NQUADS;
 		String canonicalizedDocument = (String) JsonLdProcessor.normalize(jsonLdObject, options);
 
 		// done
