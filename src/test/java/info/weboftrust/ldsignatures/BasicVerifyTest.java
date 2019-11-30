647e1ea947d06237cb6fbfcb2d759d08ad41d930
Markus Sabadello <markus@danubetech.com>
2019-11-30 22:59:14 +0400

    Remove dependency on jose4j.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- src/test/java/info/weboftrust/ldsignatures/BasicVerifyTest.java
+++ src/test/java/info/weboftrust/ldsignatures/BasicVerifyTest.java
@@ -2,10 +2,6 @@ package info.weboftrust.ldsignatures;
 
 import java.util.Collections;
 
-import org.jose4j.jwa.AlgorithmConstraints;
-import org.jose4j.jws.AlgorithmIdentifiers;
-import org.jose4j.jws.JsonWebSignature;
-
 import com.nimbusds.jose.JWSVerifier;
 import com.nimbusds.jose.Payload;
 import com.nimbusds.jose.crypto.RSASSAVerifier;
@@ -33,14 +29,6 @@ public class BasicVerifyTest extends TestCase {
 
 		JWSVerifier jwsVerifier = new RSASSAVerifier(TestUtil.testRSAPublicKey, Collections.singleton("b64"));
 		verify = jwsVerifier.verify(jwsObject.getHeader(), jwsObject.getSigningInput(), jwsObject.getParsedSignature());
-
-		JsonWebSignature jws = new JsonWebSignature();
-		jws.setAlgorithmConstraints(new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, AlgorithmIdentifiers.RSA_USING_SHA256));
-		jws.setCompactSerialization("eyJhbGciOiJSUzI1NiIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..fZRkjTTrcXdUovHjghM6JvlMhJuR1s8X1F4Uy_F4oMhZ9KtF2Zp78lYSOI7OxB5uoTu8FpQHvy-dz3N4nLhoSWAi2_HrxZG_2DyctUUB_8pRKYBmIdIgpOlEMjIreOvXyM6A32gR-PdbzoQq14yQbbfxk12jyZSwcaNu29gXnW_uO7ku1GSV_juWE5E_yIstvEB1GG8ApUGIuzRJDrAAa8KBkHN7Rdfhc8rJMOeSZI0dc_A-Y7t0M0RtrgvV_FhzM40K1pwr1YUZ5y1N4QV13M5u5qJ_lBK40WtWYL5MbJ58Qqk_-Q8l1dp6OCmoMvwdc7FmMsPigmyklqo46uyjjw");
-		jws.setPayload(unencodedPayload);
-
-		jws.setKey(TestUtil.testRSAPublicKey);
-		verify = jws.verifySignature();
 
 		// done
 
