b943a184e2c94a378dbfc858ccb8b5fc660a47c4
Markus Sabadello <markus@danubetech.com>
2021-04-29 09:56:08 +0200

    Fix BbsBlsSignature2020 term.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- src/main/java/info/weboftrust/ldsignatures/suites/BBSPlusSignature2020SignatureSuite.java
+++ src/main/java/info/weboftrust/ldsignatures/suites/BBSPlusSignature2020SignatureSuite.java
@@ -7,7 +7,7 @@ public class BBSPlusSignature2020SignatureSuite extends SignatureSuite {
 	BBSPlusSignature2020SignatureSuite() {
 
 		super(
-				"Ed25519Signature2020",
+				"BbsBlsSignature2020",
 				URI.create("https://w3id.org/security#BbsBlsSignature2020"),
 				URI.create("https://w3id.org/security#URDNA2015"),
 				URI.create("https://www.blake2.net/"),
