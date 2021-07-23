9fa3e177e0f55b5f7a2aed158fa05f81f1adc25e
Markus Sabadello <markus@danubetech.com>
2021-07-23 22:56:05 +0200

    fix: Set correct hash algorithm in suite definition.

--- src/main/java/info/weboftrust/ldsignatures/suites/Ed25519Signature2018SignatureSuite.java
+++ src/main/java/info/weboftrust/ldsignatures/suites/Ed25519Signature2018SignatureSuite.java
@@ -15,7 +15,7 @@ public class Ed25519Signature2018SignatureSuite extends SignatureSuite {
 				"Ed25519Signature2018",
 				URI.create("https://w3id.org/security#Ed25519Signature2018"),
 				URI.create("https://w3id.org/security#URDNA2015"),
-				URI.create("https://registry.ietf.org/ietf-digest-algorithms#SHA512"),
+				URI.create("http://w3id.org/digests#sha256"),
 				URI.create("http://w3id.org/security#ed25519"),
 				List.of(KeyTypeName.Ed25519),
 				Map.of(KeyTypeName.Ed25519, List.of(JWSAlgorithm.EdDSA)));
