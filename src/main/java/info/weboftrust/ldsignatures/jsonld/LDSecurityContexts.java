66934ffd61f7d01d5ac5cedc7e8a4749882e05fc
Markus Sabadello <markus@danubetech.com>
2021-11-29 02:03:33 +0100

    feat: Add more JSON-LD contexts for cryptographic suites.

--- src/main/java/info/weboftrust/ldsignatures/jsonld/LDSecurityContexts.java
+++ src/main/java/info/weboftrust/ldsignatures/jsonld/LDSecurityContexts.java
@@ -16,6 +16,11 @@ public class LDSecurityContexts {
     public static final URI JSONLD_CONTEXT_W3ID_SECURITY_V2 = URI.create("https://w3id.org/security/v2");
     public static final URI JSONLD_CONTEXT_W3ID_SECURITY_V3 = URI.create("https://w3id.org/security/v3");
     public static final URI JSONLD_CONTEXT_W3ID_SECURITY_BBS_V1 = URI.create("https://w3id.org/security/bbs/v1");
+    public static final URI JSONLD_CONTEXT_W3ID_SUITES_SECP256K1_2019_V1 = URI.create("https://w3id.org/security/suites/secp256k1-2019/v1");
+    public static final URI JSONLD_CONTEXT_W3ID_SUITES_ED25519_2018_V1 = URI.create("https://w3id.org/security/suites/ed25519-2018/v1");
+    public static final URI JSONLD_CONTEXT_W3ID_SUITES_ED25519_2020_V1 = URI.create("https://w3id.org/security/suites/ed25519-2020/v1");
+    public static final URI JSONLD_CONTEXT_W3ID_SUITES_X25519_2019_V1 = URI.create("https://w3id.org/security/suites/x25519-2019/v1");
+    public static final URI JSONLD_CONTEXT_W3ID_SUITES_JWS_2020_V1 = URI.create("https://w3id.org/security/suites/jws-2020/v1");
 
     public static final Map<URI, JsonDocument> CONTEXTS;
     public static final DocumentLoader DOCUMENT_LOADER;
@@ -34,6 +39,16 @@ public class LDSecurityContexts {
                     JsonDocument.of(MediaType.JSON_LD, LDSecurityContexts.class.getResourceAsStream("security-v3-unstable.jsonld")));
             CONTEXTS.put(JSONLD_CONTEXT_W3ID_SECURITY_BBS_V1,
                     JsonDocument.of(MediaType.JSON_LD, LDSecurityContexts.class.getResourceAsStream("security-bbs-v1.jsonld")));
+            CONTEXTS.put(JSONLD_CONTEXT_W3ID_SUITES_SECP256K1_2019_V1,
+                    JsonDocument.of(MediaType.JSON_LD, LDSecurityContexts.class.getResourceAsStream("suites-secp256k1-2019.jsonld")));
+            CONTEXTS.put(JSONLD_CONTEXT_W3ID_SUITES_ED25519_2018_V1,
+                    JsonDocument.of(MediaType.JSON_LD, LDSecurityContexts.class.getResourceAsStream("suites-ed25519-2018.jsonld")));
+            CONTEXTS.put(JSONLD_CONTEXT_W3ID_SUITES_ED25519_2020_V1,
+                    JsonDocument.of(MediaType.JSON_LD, LDSecurityContexts.class.getResourceAsStream("suites-ed25519-2020.jsonld")));
+            CONTEXTS.put(JSONLD_CONTEXT_W3ID_SUITES_X25519_2019_V1,
+                    JsonDocument.of(MediaType.JSON_LD, LDSecurityContexts.class.getResourceAsStream("suites-x25519-2019.jsonld")));
+            CONTEXTS.put(JSONLD_CONTEXT_W3ID_SUITES_JWS_2020_V1,
+                    JsonDocument.of(MediaType.JSON_LD, LDSecurityContexts.class.getResourceAsStream("suites-jws-2020.jsonld")));
 
             for (Map.Entry<URI, JsonDocument> context : CONTEXTS.entrySet()) {
                 context.getValue().setDocumentUrl(context.getKey());
