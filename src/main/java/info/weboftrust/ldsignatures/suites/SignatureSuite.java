7a67b90579a34fc62a7fa4c1c98e9cd6f90bdb83
azuzi <azeem.will@gmail.com>
2022-08-02 11:49:30 +0200

    chore: Rename getter

--- src/main/java/info/weboftrust/ldsignatures/suites/SignatureSuite.java
+++ src/main/java/info/weboftrust/ldsignatures/suites/SignatureSuite.java
@@ -42,7 +42,7 @@ public abstract class SignatureSuite {
 		return (foundAlgorithmsForKeyTypeName == null || foundAlgorithmsForKeyTypeName.size() < 1) ? null : foundAlgorithmsForKeyTypeName.get(0);
 	}
 
-	public URI getDefaultSupportedJsonLDContext() {
+	public URI getDefaultSupportedJsonLDContexts() {
 		List<URI> supportedJsonLDContexts = this.getSupportedJsonLDContexts();
 		return (supportedJsonLDContexts == null || supportedJsonLDContexts.size() < 1) ? null : supportedJsonLDContexts.get(0);
 	}
