1cf1ba1f7d4a879235f0bedaeed6a2205e8050fb
Markus Sabadello <markus@danubetech.com>
2024-01-09 13:35:15 +0100

    fix: Store context URIs as strings in JSON object

--- src/main/java/info/weboftrust/ldsignatures/signer/LdSigner.java
+++ src/main/java/info/weboftrust/ldsignatures/signer/LdSigner.java
@@ -114,7 +114,7 @@ public abstract class LdSigner<SIGNATURESUITE extends SignatureSuite> {
         if(this.getSignatureSuite().getSupportedJsonLDContexts().stream().noneMatch(jsonLDObject.getContexts()::contains)){
             URI missingJsonLDContext = this.signatureSuite.getDefaultSupportedJsonLDContext();
             if (missingJsonLDContext != null) {
-                JsonLDUtils.jsonLdAddAsJsonArray(jsonLDObject, Keywords.CONTEXT, missingJsonLDContext);
+                JsonLDUtils.jsonLdAddAsJsonArray(jsonLDObject, Keywords.CONTEXT, JsonLDUtils.uriToString(missingJsonLDContext));
             }
         }
     }
